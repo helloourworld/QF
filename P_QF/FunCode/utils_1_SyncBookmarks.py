b14cf23e6efa26511ef8b855be671680455992ba
davidyjun <davidyjun@gmail.com>
2024-03-14 14:53:26 -0300

    Syncbook

--- P_QF/FunCode/utils_1_SyncBookmarks.py
+++ P_QF/FunCode/utils_1_SyncBookmarks.py
@@ -8,6 +8,31 @@ crontab -e
 
 crontab -l
 """
+
+import scrapy
+from scrapy.http import Request
+
+class YieldSpider(scrapy.Spider):
+    name = 'yields'
+    allowed_domains = ['faa.gov']
+    start_urls = ['https://www.faa.gov/data_research']
+
+    def parse(self, response):
+        self.logger.info("A response from %s just arrived!", response.url)
+        # Your parsing logic here
+        # For XLS or XLSX files, look for links with .xls or .xlsx extensions
+        xls_links = response.css('a[href$=".xls"], a[href$=".xlsx"]::attr(href)').extract()
+        for link in xls_links:
+            yield Request(url=link, callback=self.save_xls_file)
+
+    def save_xls_file(self, response):
+        # Save the XLS file
+        filename = response.url.split('/')[-1]  # Extract the filename from the URL
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved {filename}')
+
+        
 import pandas as pd
 
 # Replace with the URL of the FAA data file (e.g., CY 2022 Passenger Boarding data)
